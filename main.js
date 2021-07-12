29a208179943b893dd40beb8813136c3c1d0ca93
mhmdali102 <mhmdali102@bk.ru>
2021-07-12 23:39:26 +0300

    button animation

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 import {icons as icons} from "./icons.js";
+
+const addBtn = document.getElementById("add");
+addBtn.addEventListener("click", createRipple);
+
+const markedBtn = document.getElementById("marked");
+markedBtn.addEventListener("click", toggleView);
+
+
 function changeIcon() {
     icons.forEach(icon => {
         const ico = document.createElement("i");
@@ -7,3 +15,26 @@ function changeIcon() {
         document.body.appendChild(ico);
     });
 };
+
+function createRipple(e) {
+    const button = e.currentTarget;
+    const span = document.createElement("span");
+    const radius = (Math.max(button.clientWidth, button.clientHeight)) / 2;
+    span.style.width = span.style.height = `${radius * 2}px`;
+    span.style.left = `${e.clientX - (button.offsetLeft + radius)}px`;
+    span.style.top = `${e.clientY - (button.offsetTop + radius)}px`;
+    span.classList.add("ripple"); 
+    const ripple = button.getElementsByClassName("ripple")[0];
+    if (ripple) {
+    ripple.remove();
+    };
+    button.appendChild(span);
+};
+
+function toggleView() {
+    const txtArea = document.getElementById("text-input");
+    const txtView = document.querySelector(".text-view");
+
+    console.log(txtView)
+
+}
