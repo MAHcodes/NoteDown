235a6dd0fbb7380ce2123af46dd3cc7f88fe805c
mahcodes <mhmdali102@bk.ru>
2023-10-06 00:05:23 +0300

    change license and add licence notice per file

--- site/app.js
+++ site/app.js
@@ -1,3 +1,26 @@
+/**
+ * NoteDown is an open source note-taking app to capture your ideas with an easy and fast way that supports formatting using Markdown syntax
+ * Copyright (C) 2023 MAHcodes
+ *
+ * This file is part of NoteDown.
+ *
+ * NoteDown is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NoteDown is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NoteDown. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * mahdotcodes@gmail.com
+ */
+
 gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline({
